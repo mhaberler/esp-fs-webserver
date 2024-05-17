07504f783d6e2ffffbd0096b3bf379f9cfe0499f
Michael Haberler <git@mah.priv.at>
2024-05-17 08:39:28 +0200

    RGB led needs fixing

--- src/simpleServer.cpp
+++ src/simpleServer.cpp
@@ -6,27 +6,21 @@
 
 FSWebServer myWebServer(FILESYSTEM, 80);
 
-#ifndef LED_BUILTIN
-#define LED_BUILTIN 2
-#endif
-
 // In order to set SSID and password open the /setup webserver page
 // const char* ssid;
 // const char* password;
-
-uint8_t ledPin = LED_BUILTIN;
 bool apMode = false;
 
 
 ////////////////////////////////  Filesystem  /////////////////////////////////////////
 void startFilesystem() {
-  // FILESYSTEM INIT
-  if ( !FILESYSTEM.begin()) {
-    Serial.println("ERROR on mounting filesystem. It will be formmatted!");
-    FILESYSTEM.format();
-    ESP.restart();
-  }
-  myWebServer.printFileList(LittleFS, Serial, "/", 2);
+    // FILESYSTEM INIT
+    if ( !FILESYSTEM.begin()) {
+        Serial.println("ERROR on mounting filesystem. It will be formmatted!");
+        FILESYSTEM.format();
+        ESP.restart();
+    }
+    myWebServer.printFileList(LittleFS, Serial, "/", 2);
 }
 
 /*
@@ -36,62 +30,83 @@ void startFilesystem() {
 */
 #ifdef ESP32
 void getFsInfo(fsInfo_t* fsInfo) {
-	fsInfo->fsName = "LittleFS";
-	fsInfo->totalBytes = LittleFS.totalBytes();
-	fsInfo->usedBytes = LittleFS.usedBytes();
+    fsInfo->fsName = "LittleFS";
+    fsInfo->totalBytes = LittleFS.totalBytes();
+    fsInfo->usedBytes = LittleFS.usedBytes();
 }
 #else
 void getFsInfo(fsInfo_t* fsInfo) {
-	fsInfo->fsName = "LittleFS";
+    fsInfo->fsName = "LittleFS";
 }
 #endif
 
+static int ledval;
+
 ////////////////////////////  HTTP Request Handlers  ////////////////////////////////////
 void handleLed() {
-  // http://xxx.xxx.xxx.xxx/led?val=1
-  if(myWebServer.hasArg("val")) {
-    int value = myWebServer.arg("val").toInt();
-    digitalWrite(ledPin, value);
-  }
+    // http://xxx.xxx.xxx.xxx/led?val=1
+    if(myWebServer.hasArg("val")) {
+        int value = myWebServer.arg("val").toInt();
+#ifdef RGB_LED
+        if (value) {
+          // digitalWrite(PIN_NEOPIXEL, HIGH); 
+            neopixelWrite(PIN_NEOPIXEL,RGB_BRIGHTNESS,0,0); // Red
+        } else {
+            // digitalWrite(PIN_NEOPIXEL, LOW);    // Turn the RGB LED off
 
-  String reply = "LED is now ";
-  reply += digitalRead(ledPin) ? "OFF" : "ON";
-  myWebServer.send(200, "text/plain", reply);
+            neopixelWrite(PIN_NEOPIXEL,0,0,0); // Off / black
+        }
+        ledval = value;
+#else
+        digitalWrite(ledPin, value);
+#endif
+    }
+
+    String reply = "LED is now ";
+#ifdef RGB_BUILTIN
+    reply += ledval ? "OFF" : "ON";
+#else
+    reply += digitalRead(ledPin) ? "OFF" : "ON";
+#endif
+    myWebServer.send(200, "text/plain", reply);
 }
 
 
-void setup(){
-  Serial.begin(115200);
+void setup() {
 
-  // FILESYSTEM INIT
-  startFilesystem();
+    Serial.begin(115200);
 
-  // Try to connect to stored SSID, start AP if fails after timeout
-  // myWebServer.setAP(WIFI_SSID, WIFI_PASSWORD);
-  myWebServer.setAP("AP", "stiwoll4711");
-  IPAddress myIP = myWebServer.startWiFi(15000);
-  Serial.println("\n");
+    // FILESYSTEM INIT
+    startFilesystem();
 
-  // Add custom page handlers to webserver
-  myWebServer.on("/led", HTTP_GET, handleLed);
-  
-  // set /setup and /edit page authentication
-  // myWebServer.setAuthentication("admin", "admin");
+    // Try to connect to stored SSID, start AP if fails after timeout
+    myWebServer.setAP(AP_SSID, AP_PASSWORD);
 
-  // Enable ACE FS file web editor and add FS info callback function
-  myWebServer.enableFsCodeEditor(getFsInfo);
+    IPAddress myIP = myWebServer.startWiFi(15000);
+    Serial.println("\n");
 
-  // Start webserver
-  myWebServer.begin();
-  Serial.print(F("ESP Web Server started on IP Address: "));
-  Serial.println(myIP);
-  Serial.println(F("Open /setup page to configure optional parameters"));
-  Serial.println(F("Open /edit page to view and edit files"));
+    // Add custom page handlers to webserver
+    myWebServer.on("/led", HTTP_GET, handleLed);
 
-  pinMode(LED_BUILTIN, OUTPUT);
+    // set /setup and /edit page authentication
+    // myWebServer.setAuthentication("admin", "admin");
+
+    // Enable ACE FS file web editor and add FS info callback function
+    myWebServer.enableFsCodeEditor(getFsInfo);
+
+    // Start webserver
+    myWebServer.begin();
+    Serial.print(F("ESP Web Server started on IP Address: "));
+    Serial.println(myIP);
+    Serial.println(F("Open /setup page to configure optional parameters"));
+    Serial.println(F("Open /edit page to view and edit files"));
+
+#ifdef RGB_LED
+    neopixelWrite(PIN_NEOPIXEL, 0, RGB_BRIGHTNESS,0); // green
+#endif
 }
 
 
 void loop() {
-  myWebServer.run();
+    myWebServer.run();
 }
